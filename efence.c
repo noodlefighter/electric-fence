1c8e6552d4598bf0bbdade344c9965ca36e350ac
noodlefighter <noodlefighter@gmail.com>
2023-04-21 11:03:02 +0800

    ElectricFence-2.2.2-posix_memalign.patch

--- efence.c
+++ efence.c
@@ -38,6 +38,7 @@
 # include <pthread.h>
 # include <semaphore.h>
 #endif
+#include <errno.h>
 
 #ifdef	malloc
 #undef	malloc
@@ -701,6 +702,27 @@ memalign(size_t alignment, size_t userSize)
 	release();
 
 	return address;
+}
+
+extern C_LINKAGE int
+posix_memalign(void **memptr, size_t alignment, size_t userSize)
+{
+	/*
+	 * Per standard, posix_memalign returns EINVAL when alignment
+	 * is not a power of two or power of sizeof(void*).  efence
+	 * doesn't check the value of alignment in memalign, but then
+	 * again, memalign was never specified very well, and on some
+	 * systems odd alignments could indeed have been allowed.
+	 */
+	if ((alignment & (alignment - 1))
+	    || alignment % sizeof (void *))
+		return EINVAL;
+
+	void *ptr = memalign (alignment, userSize);
+	if (ptr == NULL)
+		return ENOMEM;
+	*memptr = ptr;
+	return 0;
 }
 
 /*
